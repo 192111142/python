68dd63ecec2b7873b1bb0808f38dfc29387621c1
192111142 <112370088+192111142@users.noreply.github.com>
2022-08-29 14:44:54 +0530

    Add files via upload

---
+++ quadratic.py
@@ -0,0 +1,11 @@
+a=int(input("A="))
+b=int(input("B="))
+c=int(input("c="))
+#quadratic equation=-b+-(b**2-4*a*c)**0.5/2*a
+d=(b**2-4*a*c)**0.5
+e=2*a
+qua1=(-b+d)/e
+qua2=(-b+d)/e
+print(' the solution for the equation are ' , qua1,' and', qua2)
+#m.ooha
+#192111142
