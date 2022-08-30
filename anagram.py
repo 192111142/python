aa7c5808852c57234de38e76c5827ca124f30a70
192111142 <112370088+192111142@users.noreply.github.com>
2022-08-30 14:30:20 +0530

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,9 @@
+def check(s1,s2):
+    if(sorted(s1)==sorted(s2)):
+        print('the string are anagram')
+    else:
+        print('the strings are not anagram')
+ s1=str(input("enter the word:"))
+ s2=str(input("enter another number word:"))
+ checks(s1,s2)
+            
