3760e52584ab7ff26279537b4b0b99ea2b3309a6
192111142 <112370088+192111142@users.noreply.github.com>
2022-08-30 12:38:22 +0530

    Add files via upload

---
+++ integers.py
@@ -0,0 +1,6 @@
+  1st=[]
+  num=int(input('how many numbers: '))
+  for n in range(num):
+      number=int(input('enter number '))
+      1st,append(numbers)
+ print("maximum element in the list is :",max(1st))
